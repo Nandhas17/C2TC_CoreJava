d45aeefc743a6273803a432f410600206c6dd9ee
vinoth <vinoth@DESKTOP-NBLR8DJ>
2024-07-29 13:23:11 -0700

    POLYMORPHISM

---
+++ Runpoly/src/com/tns/daysix/C.java
@@ -0,0 +1,16 @@
+package com.tns.daysix;
+
+public class C {
+
+	public static void main(String[] args) {
+
+		Rpoly p = new Rpoly();
+		p.a(1);
+		B b = new B();
+		b.a(1);
+		Rpoly a = new B();
+		a.a(1);
+		
+	}
+
+}
---
+++ Runpoly/src/com/tns/daysix/Rpoly.java
@@ -0,0 +1,15 @@
+package com.tns.daysix;
+
+public class Rpoly {
+	void a(int a) {
+		System.out.println("class 1");
+	
+	}
+
+}
+
+class B extends Rpoly{
+	void a(int a) {
+		System.out.println("class 2");
+	}
+}
